571a3ce29f22f3d8717f4fd2d4e3b6341f553693
JorickM <21138@stu.ipbeja.pt>
2021-11-09 16:43:21 +0000

    Add some classes.

---
+++ 21138_test/app/src/main/java/com/example/projeto_pas/User.java
@@ -0,0 +1,72 @@
+package com.example.projeto_pas;
+
+public class User {
+    private long codUser;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private int typeUser;
+
+    public User(long codUser, String name, String email, String lastName, String password, int typeUser) {
+        this.codUser = codUser;
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+        this.password = password;
+        this.typeUser = typeUser;
+    }
+
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getCodUser() {
+        return codUser;
+    }
+
+    public void setCodUser(long codUser) {
+        this.codUser = codUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(int typeUser) {
+        this.typeUser = typeUser;
+    }
+}
