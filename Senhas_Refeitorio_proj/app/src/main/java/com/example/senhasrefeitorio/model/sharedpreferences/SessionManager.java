45c7fd37521a0804025c648047fea5a45d388c8d
ricardooooooo <21140@stu.ipbeja.pt>
2022-01-25 16:37:50 +0000

    SessionManager

--- Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/model/sharedpreferences/SessionManager.java
+++ Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/model/sharedpreferences/SessionManager.java
@@ -6,13 +6,18 @@ import android.content.SharedPreferences;
 import com.example.senhasrefeitorio.model.User;
 
 public class SessionManager {
+
     private SessionManager() {}
 
     private static SharedPreferences sharedPreferences;
     private static final String FILENAME = "sessionSharedPrefs";
     private static final String KEY_USER_CODE = "userID";
     private static final String KEY_USER_EMAIL = "email";
-
+    private static final String KEY_USER_NAME = "userName";
+    private static final String KEY_USER_LAST_NAME = "userLastName";
+    private static final String KEY_USER_URL = "userURL";
+    private static final String KEY_USER_TYPE = "userType";
+    ;
 
     private static SharedPreferences getSharedPreferences(Context context) {
         if (sharedPreferences == null) {
@@ -27,15 +32,29 @@ public class SessionManager {
 
     public static User getActiveSession(Context context) {
         if (getSharedPreferences(context).contains(KEY_USER_EMAIL)) {
-            User user = new User(getSharedPreferences(context).getString(KEY_USER_EMAIL, ""), "");
+            SharedPreferences sharedPreferences = getSharedPreferences(context);
+            long code = sharedPreferences.getLong(KEY_USER_CODE, 0);
+            String name = sharedPreferences.getString(KEY_USER_NAME, "");
+            String lastName = sharedPreferences.getString(KEY_USER_LAST_NAME, "");
+            String email = sharedPreferences.getString(KEY_USER_EMAIL, "");
+            String url = sharedPreferences.getString(KEY_USER_URL, "");
+            int type = sharedPreferences.getInt(KEY_USER_TYPE, 0);
+            User user = new User(code, name, lastName, email, "", url, type);
             return user;
         }
         return null;
     }
 
     public static void saveSession(Context context, User user) {
-        getSharedPreferences(context).edit().putString(KEY_USER_EMAIL, user.getEmail()).apply();
+        getSharedPreferences(context).edit()
+                .putLong(KEY_USER_CODE, user.getCodUser())
+                .putString(KEY_USER_NAME, user.getName())
+                .putString(KEY_USER_LAST_NAME, user.getLastName())
+                .putString(KEY_USER_EMAIL, user.getEmail())
+                .putString(KEY_USER_URL, user.getUrl())
+                .putInt(KEY_USER_TYPE, user.getTypeUser())
+                .apply();
     }
-
+//KEY_USER_CODE , user.getCodUser()
 
 }
--- Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/view/MainMenuFragment.java
+++ Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/view/MainMenuFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -46,10 +47,10 @@ public class MainMenuFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setHasOptionsMenu(true);
+        mLoginViewModel = new ViewModelProvider(this).get(LoginActivityViewModel.class);
+
 
         userFromLogin = SessionManager.getActiveSession(getActivity());
-
-        mLoginViewModel = new ViewModelProvider(this).get(LoginActivityViewModel.class);
 
         ImageView imageView;
         imageView = view.findViewById(R.id.imgUser);
