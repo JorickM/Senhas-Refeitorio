70749b95ae33d8a9d3f3702a429c896339c84910
JorickM <21138@stu.ipbeja.pt>
2022-01-30 23:40:18 +0000

    Generate QR code

--- Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/view/PurchaseAdapter.java
+++ Senhas_Refeitorio_proj/app/src/main/java/com/example/senhasrefeitorio/view/PurchaseAdapter.java
@@ -1,9 +1,12 @@
 package com.example.senhasrefeitorio.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.senhasrefeitorio.R;
 import com.example.senhasrefeitorio.model.PurchaseWithMeal;
 import com.example.senhasrefeitorio.model.Weekday;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +49,18 @@ public class PurchaseAdapter extends RecyclerView.Adapter<PurchaseAdapter.ViewHo
         holder.root.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mostrar qr code
+                try {
+                    MultiFormatWriter writer = new MultiFormatWriter();
+
+                    BitMatrix matrix = writer.encode(purchase.getMealWithWeekday().getMeal().getMainDish() + " " + purchase.getMealWithWeekday().getWeekday().getDate(), BarcodeFormat.QR_CODE, 350, 350);
+
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    Bitmap bitmap = encoder.createBitmap(matrix);
+
+                    holder.imageView.setImageBitmap(bitmap);
+                }catch (Exception e){
+
+                }
             }
         });
     }
@@ -61,12 +79,14 @@ public class PurchaseAdapter extends RecyclerView.Adapter<PurchaseAdapter.ViewHo
         View root;
         TextView txtPurchWeekday1;
         TextView txtPurchMeal1;
+        ImageView imageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.root = itemView;
             this.txtPurchWeekday1 = this.root.findViewById(R.id.txtPurchWeekday);
             this.txtPurchMeal1 = this.root.findViewById(R.id.txtPurchMeal);
+            this.imageView = this.root.findViewById(R.id.imageView2);
         }
     }
 }
