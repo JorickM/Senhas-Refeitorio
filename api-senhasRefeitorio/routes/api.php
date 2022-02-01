0db3d8a0745525bbcac8e136d3336cbad89d8a32
ricardooooooo <21140@stu.ipbeja.pt>
2022-02-01 05:06:14 +0000

    power point

--- api-senhasRefeitorio/routes/api.php
+++ api-senhasRefeitorio/routes/api.php
@@ -22,33 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::resource('logins','LoginController');
-//Route::get('logins/getLoginByEmailPassword', 'LoginController@getLoginByEmailPassword');
 
-//Route::get('logins',[LoginController::class,'index']);
-//Route::get('logins/{codUser}',[LoginController::class,'show']);
-//Route::post('logins/create',[LoginController::class,'create']);
-//Route::put('logins/',[LoginController::class,'update']);
-//Route::delete('logins/{codUser}',[LoginController::class,'destroy']);
 Route::post('logins', [LoginController::class, 'postLoginByEmailPassword']);
-//Route::get('logins/getAll',[LoginController::class,'getAll']);
 
-
-//Route::post('weekdays',[WeekdayController::class,'create']);
 Route::get('weekdays',[WeekdayController::class,'getAllWeekdays']);
-//Route::get('weekdays/{codWeekday}',[WeekdayController::class,'show']);
-//Route::put('weekdays/',[WeekdayController::class,'update']);
-//Route::delete('weekdays/{codWeekday}',[WeekdayController::class,'destroy']);
 
 Route::get('meals',[MealController::class,'getAllMeals']);
-//Route::get('meals/{codMeal}',[MealController::class,'show']);
-//Route::post('meals',[MealController::class,'create']);
-//Route::put('meals/',[MealController::class,'update']);
-//Route::delete('meals/{codMeal}',[MealController::class,'destroy']);
 
 Route::get('purchases/all',[PurchaseController::class,'showAll']);
 Route::get('purchases/byUser/{codUser}',[PurchaseController::class,'getPurchasesForUser']);
-//Route::get('purchases/{codPurchase}',[PurchaseController::class,'show']);
 Route::post('purchases',[PurchaseController::class,'addNew']);
 Route::put('purchases/',[PurchaseController::class,'updatePurchase']);
-//Route::delete('purchases/{codPurchase}',[PurchaseController::class,'destroy']);
